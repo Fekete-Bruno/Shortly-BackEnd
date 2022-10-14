aa8db150f570b9b6c69618916f80b6011b2d21c5
Fekete-Bruno <brunohffekete@gmail.com>
2022-10-13 22:07:12 -0300

    refactor: removed get status from signup

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -4,7 +4,6 @@ import validateUser from "../middlewares/user.validation.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/status", (req,res)=>{res.sendStatus(200)});
 userRouter.post("/signup",validateUser,postUsers);
 
 export default userRouter;
