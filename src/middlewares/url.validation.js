e6c30bed1eaebb41d5feca5c25bf4dabcce32f62
Fekete-Bruno <brunohffekete@gmail.com>
2022-10-15 18:54:51 -0300

    feat: separated session validation

--- src/middlewares/url.validation.js
+++ src/middlewares/url.validation.js
@@ -1,41 +1,5 @@
 import connection from "../database/database.js";
 import urlSchema from "../schemas/url.schema.js";
-import dayjs from 'dayjs';
-
-
-async function validateSession(req,res,next){
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
-    const errorMessage = 'Invalid user token, log-in again!'; 
-
-    if (!token) return res.status(401).send(errorMessage);
-
-    let user_id = '';
-    try {
-        const query = await connection.query(`
-            SELECT user_id,created_at 
-            FROM sessions 
-            WHERE token = $1
-        `,[token]);
-        user_id = query.rows[0]?.user_id; 
-
-        if(!user_id){
-            return res.status(401).send(errorMessage);
-        }
-
-        if(dayjs().diff(dayjs(query.rows[0]?.created_at),'h')>2){
-            await connection.query(`DELETE FROM sessions WHERE token = $1;`,[token]);
-            return res.status(401).send('Expired token!');
-        }
-    } catch (error) {
-        console.error(error);
-        return res.sendStatus(500);
-    }
-
-    res.locals.user_id = user_id;
-
-    next();
-}
 
 function validateUrl(req,res,next){
     const { url } = req.body;
@@ -74,4 +38,4 @@ async function validateUserUrl(req,res,next){
     next();
 }
 
-export {validateSession,validateUrl,validateUserUrl};
+export {validateUrl,validateUserUrl};
--- src/routers/url.router.js
+++ src/routers/url.router.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { deleteUrl, getUrl, openUrl, postUrl } from '../controllers/url.controller.js';
-import { validateSession, validateUrl, validateUserUrl} from '../middlewares/url.validation.js';
+import validateSession from '../middlewares/session.validation.js';
+import { validateUrl, validateUserUrl} from '../middlewares/url.validation.js';
+
 
 const urlRouter = express.Router();
 
