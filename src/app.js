40f48f9c353d052d7ea107242d8cbb5d49701648
Fekete-Bruno <brunohffekete@gmail.com>
2022-10-15 22:08:59 -0300

    feat: GET ranking is working

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import userRouter from './routers/user.router.js';
 import loginRouter from './routers/login.router.js';
 import urlRouter from './routers/url.router.js';
+import rankingRouter from './routers/ranking.router.js';
 
 const app = express();
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(loginRouter);
 app.use(urlRouter);
+app.use(rankingRouter);
 
 export default app;
---
+++ src/controllers/ranking.controller.js
@@ -0,0 +1,29 @@
+import connection  from "../database/database.js";
+
+async function getRanking(req,res){
+    let body = [] 
+
+    try {
+        const query = await connection.query(`
+            SELECT 
+                users.id,
+                name,
+                COALESCE(SUM("visitCount"),0) AS "visitCount",
+                COUNT(url) AS "linksCount"
+            FROM users 
+            LEFT JOIN links 
+            ON links."userId" = users.id
+            GROUP BY users.id
+            ORDER BY "visitCount" DESC
+            LIMIT 10;
+        `);
+        body = query.rows;
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+
+    return res.send(body);
+}
+
+export default getRanking;
